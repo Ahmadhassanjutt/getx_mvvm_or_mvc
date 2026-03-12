fc5e1a498635b8199a75bf0cc14f87899a7c3a14
A4AhmadNKU <ahmad.hassan@nkutechnologies.com>
2026-03-12 11:32:17 +0500

    new

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -1,56 +1,73 @@
-
 import 'dart:io';
-
-import 'package:getx_and_mvvm_or_mvc/data/network/base_api_services.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:getx_and_mvvm_or_mvc/data/network/base_api_services.dart';
 import '../app_exceptions.dart';
 
 class NetworkApiServices extends BaseApiServices {
 
   @override
-  Future<dynamic> getApi(String url)  async {
+  Future<dynamic> getApi(String url) async {
 
     dynamic responseJson;
+
     try {
-      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
+      final response = await http
+          .get(Uri.parse(url))
+          .timeout(const Duration(seconds: 10));
+
       responseJson = returnResponse(response);
 
-    }on SocketException {
-      throw InternetExceptions("");
-    }on RequestTimeOut {
-      throw RequestTimeOut("");
-
-  }
-        return responseJson;
-}
-
-  @override
-  Future<dynamic> postApi(var data, String url)  async {
-
-    dynamic responseJson;
-    try {
-      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
-      responseJson = returnResponse(response);
-
-    }on SocketException {
-      throw InternetExceptions("");
-    }on RequestTimeOut {
-      throw RequestTimeOut("");
-
+    } on SocketException {
+      throw InternetExceptions("No Internet Connection");
+    } on HttpException {
+      throw FetchDataException("Error while fetching data");
     }
+
     return responseJson;
   }
 
-dynamic returnResponse(http.Response response){
-    switch(response.statusCode){
+  @override
+  Future<dynamic> postApi(String url, dynamic data) async {
+
+    dynamic responseJson;
+
+    try {
+      final response = await http
+          .post(
+        Uri.parse(url),
+        body: data,
+      )
+          .timeout(const Duration(seconds: 10));
+
+      responseJson = returnResponse(response);
+
+    } on SocketException {
+      throw InternetExceptions("No Internet Connection");
+    } on HttpException {
+      throw FetchDataException("Error while posting data");
+    }
+
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+
+    switch (response.statusCode) {
+
       case 200:
-        dynamic responseJson = response.body;
-        return responseJson;
+        return response.body;
+
       case 400:
         throw InvalidUrl(response.body.toString());
+
+      case 500:
+        throw ServerException(response.body.toString());
+
       default:
-        throw FetchDataException("Error accured while communicating with server with status code ${response.statusCode.toString()}");
+        throw FetchDataException(
+          "Error occurred while communicating with server with status code ${response.statusCode}",
+        );
     }
-}
+  }
 }
