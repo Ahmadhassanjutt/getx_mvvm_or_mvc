3a5ce9d0c89092f0e47bf1a2504fece663d1531a
A4AhmadNKU <ahmad.hassan@nkutechnologies.com>
2026-03-28 14:22:24 +0500

    new

--- lib/view_models/controller/login_controller.dart
+++ lib/view_models/controller/login_controller.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:getx_and_mvvm_or_mvc/repositoory/login_repository/login_repository.dart';
 class LoginController extends GetxController{
 
+  final _api = LoginRepository();
   final emailController = TextEditingController().obs;
   final passwordController = TextEditingController().obs;
 
@@ -10,6 +12,12 @@ class LoginController extends GetxController{
 
 
   void loginApi(){
+
+    // _api.loginApi(data).then((onValue){
+    //
+    // }).onError((error, stackTrace){
+    //
+    // });
 
   }
 
